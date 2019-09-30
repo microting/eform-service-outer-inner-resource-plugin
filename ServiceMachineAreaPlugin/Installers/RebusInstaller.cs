16f70921330768221bc9004f8f6c616b96e6229a
René Schultz Madsen <rm@microting.dk>
2019-09-30 10:24:27 +0200

    Updating: - Microting.eForm to 3.0.326 - Microting.eFormMachineAreaBase to 1.1.67 Swithcing from MySQL to RabbitMq as transporter for Rebus.

--- ServiceMachineAreaPlugin/Installers/RebusInstaller.cs
+++ ServiceMachineAreaPlugin/Installers/RebusInstaller.cs
@@ -42,31 +42,16 @@ namespace ServiceMachineAreaPlugin.Installers
 
         public void Install(IWindsorContainer container, IConfigurationStore store)
         {
-            if (connectionString.ToLower().Contains("convert zero datetime"))
-            {
-                Configure.With(new CastleWindsorContainerAdapter(container))
-                    .Logging(l => l.ColoredConsole())
-                    .Transport(t => t.UseMySql(connectionStringOrConnectionOrConnectionStringName: connectionString, tableName: "Rebus", inputQueueName: "machinearea-input"))
-                    .Options(o =>
-                    {
-                        o.SetMaxParallelism(maxParallelism);
-                        o.SetNumberOfWorkers(numberOfWorkers);
-                    })
-                    .Start();
-            }
-            else
-            {
-                Configure.With(new CastleWindsorContainerAdapter(container))
-                    .Logging(l => l.ColoredConsole())
-                    .Transport(t => t.UseSqlServer(connectionString: connectionString, inputQueueName: "machinearea-input"))
-                    //.Transport(t => t.UseSqlServer(connectionStringOrConnectionStringName: connectionString, tableName: "Rebus", inputQueueName: "eformsdk-input"))
-                    .Options(o =>
-                    {
-                        o.SetMaxParallelism(maxParallelism);
-                        o.SetNumberOfWorkers(numberOfWorkers);
-                    })
-                    .Start();
-            }
+            Configure.With(new CastleWindsorContainerAdapter(container))
+                .Logging(l => l.ColoredConsole())
+                .Transport(t => t.UseRabbitMq("amqp://admin:password@localhost", "eform-angular-trashinspection-plugin"))
+                .Options(o =>
+                {
+                    o.SetMaxParallelism(maxParallelism);
+                    o.SetNumberOfWorkers(numberOfWorkers);
+                    o.LogPipeline(verbose:true);
+                })
+                .Start();
             
         }
     }
