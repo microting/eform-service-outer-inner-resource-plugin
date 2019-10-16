6d3eb0dff083c195edc341c494d1864b4a4f0191
René Schultz Madsen <rm@microting.dk>
2019-10-16 10:41:56 +0200

    Renaming the tests.

--- install.sh
+++ install.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-if [ ! -d "/var/www/microting/eform-service-machinearea-plugin" ]; then
+if [ ! -d "/var/www/microting/eform-service-outer-inner-resource-plugin" ]; then
   cd /var/www/microting
   su ubuntu -c \
-  "git clone https://github.com/microting/eform-service-machinearea-plugin.git -b stable"
+  "git clone https://github.com/microting/eform-service-outer-inner-resource-plugin.git -b stable"
 fi
 
-cd /var/www/microting/eform-service-machinearea-plugin
+cd /var/www/microting/eform-service-OuterInnerResource-plugin
 su ubuntu -c \
-"dotnet restore ServiceMachineAreaPlugin.sln"
+"dotnet restore ServiceOuterInnerResourcePlugin.sln"
 
 echo "################## START GITVERSION ##################"
 export GITVERSION=`git describe --abbrev=0 --tags | cut -d "v" -f 2`
 echo $GITVERSION
 echo "################## END GITVERSION ##################"
 su ubuntu -c \
-"dotnet publish ServiceMachineAreaPlugin.sln -o out /p:Version=$GITVERSION --runtime linux-x64 --configuration Release"
+"dotnet publish ServiceOuterInnerResourcePlugin.sln -o out /p:Version=$GITVERSION --runtime linux-x64 --configuration Release"
 
 su ubuntu -c \
 "mkdir -p /var/www/microting/eform-debian-service/MicrotingService/MicrotingService/out/Plugins/"
 
 su ubuntu -c \
-"cp -av /var/www/microting/eform-service-machinearea-plugin/ServiceMachineAreaPlugin/out /var/www/microting/eform-debian-service/MicrotingService/MicrotingService/out/Plugins/ServiceMachineAreaPlugin"
-./rabbitmqadmin declare queue name=eform-service-machinearea-plugin durable=true
+"cp -av /var/www/microting/eform-service-OuterInnerResource-plugin/ServiceOuterInnerResourcePlugin/out /var/www/microting/eform-debian-service/MicrotingService/MicrotingService/out/Plugins/ServiceOuterInnerResourcePlugin"
+./rabbitmqadmin declare queue name=eform-service-OuterInnerResource-plugin durable=true
