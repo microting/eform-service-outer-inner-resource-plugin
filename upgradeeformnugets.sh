cbd7f55f5420cae60ea1eda58f3c98f16f2464d3
René Schultz Madsen <rm@microting.dk>
2020-07-28 09:01:58 +0000

    Updating the service plugin to include the basepn.

--- upgradeeformnugets.sh
+++ upgradeeformnugets.sh
@@ -7,6 +7,7 @@ if (( "$GIT_STATUS" > 0 )); then
 
 	dotnet add ServiceOuterInnerResourcePlugin.csproj package Microting.eForm
 	dotnet add ServiceOuterInnerResourcePlugin.csproj package Microting.eFormOuterInnerResourceBase
+	dotnet add ServiceOuterInnerResourcePlugin.csproj package Microting.WindowsService.BasePn
 
 	EFORM_VERSION=`dotnet list package | grep 'Microting.eForm ' | cut -c64-71`
 	EFORM_PLUGINBASE_VERSION=`dotnet list package | grep 'Microting.eFormOuterInnerResourceBase' | cut -c64-71`
@@ -27,7 +28,7 @@ if (( "$GIT_STATUS" > 0 )); then
 		NEW_GIT_VERSION="v$MAJOR_VERSION.$MINOR_VERSION.$BUILD_VERSION"
 		git tag "$NEW_GIT_VERSION"
 		git push --tags
-		git push	
+		git push
 		echo "Updated Microting eForm to ${EFORM_VERSION} and pushed new version ${NEW_GIT_VERSION}"
 	fi
 else
